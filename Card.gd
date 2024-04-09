dd96017ebeead6dcfedc6b51f7816e9777710701
neuroticAxolotl <lea.rajala.02@gmail.com>
2024-04-09 17:08:27 +0300

    basic card code done(ish)

--- Card.gd
+++ Card.gd
@@ -1,12 +1,12 @@
 extends TextureRect
 
 # set by global function create_card()
-var rank:int = 1
-var suit = Global.Suits.SPADE
+var rank:int = 6
+var suit = Global.Suits.CLUB
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	update_values()
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,21 +15,35 @@ func _process(delta):
 
 
 func update_values():
-	var children = get_children()
 	var suit_icons = []
 	var rank_labels = []
 	
-	for child in children:
+	# get child nodes that need to display suit & rank
+	for child in get_children():
 		if child.name.begins_with("SuitIcon"):
 			suit_icons.append(child)
 		elif child.name.begins_with("RankLabel"):
 			rank_labels.append(child)
+		elif child.name.begins_with("Decoration"):
+			for grandchild in child.get_children():
+				if grandchild.name.begins_with("SuitIcon"):
+					suit_icons.append(grandchild)
 	
-	match rank:
-		1:
+	for label in rank_labels:
+		if rank == 1:
 			# set decoration here
-			for label in rank_labels:
-				label.text = "A"
-	
+			label.text = "A"
+		if rank > 1 and rank < 11:
+			label.text = str(rank)
+		if rank == 11:
+			label.text = "J"
+		if rank == 12:
+			label.text = "Q"
+		if rank == 13:
+			label.text = "K"
+		
+		label.label_settings.font_color = Global.suit_colors[suit]
+
 	for node in suit_icons:
-		node.texture = suit
+		node.texture = Global.suit_icons[suit]
+
--- global.gd
+++ global.gd
@@ -3,8 +3,20 @@ extends Node
 var card_scene = preload("res://card.tscn")
 
 enum Suits {SPADE, HEART, CLUB, DIAMOND}
-var suit_icons = {Suits.SPADE: preload("res://spade.tres")}
 
+var suit_icons = {
+	Suits.SPADE: preload("res://spade.tres"), 
+	Suits.HEART: preload("res://heart.tres"), 
+	Suits.CLUB: preload("res://club.tres"), 
+	Suits.DIAMOND: preload("res://diamond.tres"),
+	}
+
+var suit_colors = {
+	Suits.SPADE: Color("000000"), 
+	Suits.HEART: Color("ff0000"), 
+	Suits.CLUB: Color("404b4f"), 
+	Suits.DIAMOND: Color("ff7800"),
+	}
 
 
 func CreateCard(suit, rank):
